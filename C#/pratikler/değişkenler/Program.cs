2d4fd61bc51167f282354187f3e0a852f262323b
Osman TÜYSÜZ <osman.tuysuz@bil.omu.edu.tr>
2021-08-25 12:41:25 +0300

    app.patika.dev C# 101 Ödev-2 dosyaları yüklendi.

---
+++ C#/pratikler/değişkenler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace değişkenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 2;
+            string metin = "2";
+
+            int degistirTopla = sayi + Convert.ToInt32(metin);
+            Console.WriteLine(degistirTopla);
+        }
+    }
+}
