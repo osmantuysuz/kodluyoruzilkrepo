74ae074ab7bb75b8c4346f86874578b0437c108a
Osman TÜYSÜZ <osman.tuysuz@bil.omu.edu.tr>
2021-08-25 11:17:15 +0300

    C# kursu için proje dosyaları oluşturuldu.

---
+++ C#/pratikler/console-programlama/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
