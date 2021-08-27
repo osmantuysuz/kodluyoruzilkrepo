62429de83888876ab7c481e6d0c8bff633320496
Osman TÜYSÜZ <osman.tuysuz@bil.omu.edu.tr>
2021-08-27 16:07:27 +0300

    Ödev-1'in ilk sorusu.

---
+++ C#/pratikler/Odev1/Soru1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Soru1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int girilecekSayiAdedi;
+            int[] girilenSayilar;
+
+            Console.WriteLine("****Lütfen pozitif bir sayı giriniz!****");
+            Console.WriteLine("Kaç adet sayı gireceksiniz: ");
+            girilecekSayiAdedi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Anlaşıldı. Gireceğiniz sayılardan çift olanları size göstereceğim...");
+
+            for (int i = 0; i < girilecekSayiAdedi; i++)
+            {
+                Console.WriteLine(i + ". sayıyı giriniz: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                if(sayi%2==0){
+                    girilenSayilar[i] = sayi;
+                }
+            }
+            
+            foreach (var item in girilenSayilar){
+                Console.WriteLine(girilenSayilar[item]);
+            }
+        }
+    }
+}
